f8f233114bfbc7a12ef3bcb07af019d9e266e88e
lee1 <dltmdwns6986@naver.com>
2023-10-25 16:24:14 +0900

    za

--- lib/concertDetails.dart
+++ lib/concertDetails.dart
@@ -18,25 +18,153 @@ class ConcertDetails extends StatelessWidget {
         body: TabBarView(
           children: [
             // 첫 번째 탭 (상세 정보)
-            Container(
-              child: Column(
-                children: [
-                  // 상단 이미지
-                  Image.asset(
-                    'assets/bus_sample1.jpg',
-                    height: 300, // 이미지 높이 설정
-                    width: 600,// 이미지가 위젯에 맞게 조절되도록 설정
-                  ),
+            SingleChildScrollView(
+              child: Container(
 
-                  // 이어서 다른 위젯들을 추가할 수 있습니다.
-                  // ...
-                ],
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // 상단 이미지
+                    Image.asset(
+                      'assets/bus_sample1.jpg',
+                      height: 130,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(height: 30), // 간격 추가
+                    Text(
+                      'INTRODUCTION',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10), // 간격 추가
+                    Container(
+                      height: 1.0,
+                      width: double.infinity,
+                      color: Colors.black.withOpacity(0.1),
+                    ),
+                    SizedBox(height: 10), // 간격 추가
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "  기본정보",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+
+                      color: Color(0xFFdcdcdc),
+                      height: 250,
+                      width: 400,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 20),
+                              child: Image.asset(
+                                'assets/기본.jpg',
+                                height: 150,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Nick",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 30),
+                                Container(
+                                  height: 1.0,
+                                  width: 200,
+                                  color: Colors.black.withOpacity(0.1),
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  "장소          place",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                Text(
+                                  "시간          time",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                Text(
+                                  "출연          musision",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                Text(
+                                  "장르          rock",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      width: double.infinity,
+                      child: Row(
+                        children: [
+                          Container(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "  댓글",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.refresh),
+                            onPressed: () {
+                              // Add your refresh logic here
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      width: double.infinity,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Write a comment...',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          prefixIcon: Icon(Icons.mode_comment),
+                        ),
+                        maxLines: null, // Allow multiple lines for the comment
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-
             // 두 번째 탭 (공연 후기)
-            Center(
-              child: Text('여기에 공연 후기가 들어갑니다.'),
+            SingleChildScrollView(
+              child: Center(
+                child: Text('여기에 공연 후기가 들어갑니다.'),
+              ),
             ),
           ],
         ),
