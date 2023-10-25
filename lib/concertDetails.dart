c84406112cce0033244d57b415e81166dffd17b7
lee1 <dltmdwns6986@naver.com>
2023-10-25 18:26:32 +0900

    za

--- lib/concertDetails.dart
+++ lib/concertDetails.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ConcertDetails extends StatelessWidget {
+class ConcertDetails extends StatefulWidget {
+  @override
+  _ConcertDetailsState createState() => _ConcertDetailsState();
+}
+
+class _ConcertDetailsState extends State<ConcertDetails> {
+  Map<String, dynamic>? buskingData; // 받아온 버스킹 데이터를 저장할 변수
+
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getBuskingDetails(String buskingID) async {
+    return await FirebaseFirestore.instance.collection('busking').doc(buskingID).get();
+  }
+
+  Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>> getBuskingImages(String buskingID) async {
+    return await FirebaseFirestore.instance.collection('busking').doc(buskingID).collection('image').get().then((snapshot) {
+      return snapshot.docs;
+    });
+  }
+
+  List<QueryDocumentSnapshot<Map<String, dynamic>>>? buskingImages;
+
+  Future<void> loadBuskingImages() async {
+    buskingImages = await getBuskingImages('rSGUvxgxllJ1t3qDchUU');
+    setState(() {});
+
+    // 각 이미지의 NAME 필드 값을 출력합니다.
+    if (buskingImages != null) {
+      for (var image in buskingImages!) {
+        print('이미지 이름: ${image['path']}');
+      }
+    }
+  }
+
+
+  Future<void> loadBuskingData() async {
+    buskingData = null;
+    DocumentSnapshot<Map<String, dynamic>> buskingSnapshot = await getBuskingDetails('v395OaqYv58fY7ewS7lV');
+    setState(() {
+      buskingData = buskingSnapshot.data();
+    });
+
+    await loadBuskingImages(); // loadBuskingImages를 호출합니다.
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadBuskingData();
+    loadBuskingImages();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2, // 탭의 개수 (상세 정보와 공연 후기)
+      length: 2,
       child: Scaffold(
         appBar: AppBar(
           title: Text('공연 상세 페이지'),
@@ -17,150 +68,39 @@ class ConcertDetails extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            // 첫 번째 탭 (상세 정보)
             SingleChildScrollView(
               child: Container(
-
+                padding: EdgeInsets.all(16.0),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    // 상단 이미지
-                    Image.asset(
-                      'assets/bus_sample1.jpg',
-                      height: 130,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-                    SizedBox(height: 30), // 간격 추가
                     Text(
-                      'INTRODUCTION',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 10), // 간격 추가
-                    Container(
-                      height: 1.0,
-                      width: double.infinity,
-                      color: Colors.black.withOpacity(0.1),
-                    ),
-                    SizedBox(height: 10), // 간격 추가
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "  기본정보",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-
-                      color: Color(0xFFdcdcdc),
-                      height: 250,
-                      width: 400,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 20),
-                              child: Image.asset(
-                                'assets/기본.jpg',
-                                height: 150,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Nick",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                SizedBox(height: 30),
-                                Container(
-                                  height: 1.0,
-                                  width: 200,
-                                  color: Colors.black.withOpacity(0.1),
-                                ),
-                                SizedBox(height: 20),
-                                Text(
-                                  "장소          place",
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                  ),
-                                ),
-                                Text(
-                                  "시간          time",
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                  ),
-                                ),
-                                Text(
-                                  "출연          musision",
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                  ),
-                                ),
-                                Text(
-                                  "장르          rock",
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      width: double.infinity,
-                      child: Row(
-                        children: [
-                          Container(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "  댓글",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.refresh),
-                            onPressed: () {
-                              // Add your refresh logic here
-                            },
-                          ),
-                        ],
-                      ),
+                      '아티스트 아이디: ${buskingData?['aritistId']}',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
                     SizedBox(height: 10),
-                    Container(
-                      width: double.infinity,
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Write a comment...',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          prefixIcon: Icon(Icons.mode_comment),
-                        ),
-                        maxLines: null, // Allow multiple lines for the comment
-                      ),
+                    Text(
+                      '버스킹 설명: ${buskingData?['description']}',
+                      style: TextStyle(fontSize: 16),
                     ),
+                    SizedBox(height: 10),
+                    Text(
+                      '스팟 아이디: ${buskingData?['spotId']}',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(height: 10), // Add some spacing
+                    for (var index = 0; index < buskingImages!.length; index++)
+                      Image.asset(
+                        '${buskingImages![index]['path']}',
+                        height: 100,
+                        width: 100,
+                      ),
+
+
                   ],
                 ),
               ),
             ),
-            // 두 번째 탭 (공연 후기)
             SingleChildScrollView(
               child: Center(
                 child: Text('여기에 공연 후기가 들어갑니다.'),
