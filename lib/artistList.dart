1eed1fb11641e038c138b307cdc78a07463b2302
CS Lee <ebdh56@naver.com>
2023-10-26 11:00:45 +0900

    메인 버려

---
+++ lib/artistList.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ArtistList extends StatefulWidget {
+  const ArtistList({super.key});
+
+  @override
+  State<ArtistList> createState() => _ArtistListState();
+}
+
+class _ArtistListState extends State<ArtistList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ㅁㄴㅇㄹ"),
+      ),
+    );
+  }
+}
