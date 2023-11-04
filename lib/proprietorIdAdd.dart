70aeea9ccc9726c06b442e08bacb08e3e2bebd25
dlcks <ebdh56@naver.com>
2023-11-05 03:19:46 +0900

    으앙..

--- lib/proprietorIdAdd.dart
+++ lib/proprietorIdAdd.dart
@@ -18,10 +18,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 import 'package:uuid/uuid.dart';
 import 'package:flutter/services.dart';
-import 'baseBar.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geocoding/geocoding.dart';
@@ -37,6 +35,8 @@ class ProprietorAdd extends StatefulWidget {
 }
 
 class _ProprietorAddState extends State<ProprietorAdd> {
+  final FirebaseFirestore fs = FirebaseFirestore.instance;
+
   File? _selectedImage;
 
   final TextEditingController _proprietorName = TextEditingController(); // 상호명
@@ -48,15 +48,18 @@ class _ProprietorAddState extends State<ProprietorAdd> {
   final TextEditingController _commerAddr = TextEditingController(); // 공간 주소
   final TextEditingController _commerDetaillAddr = TextEditingController(); // 공간 상세주소
 
-  final TextEditingController _genreSelf = TextEditingController(); // 공연 가능한 장르 직접 입력
 
-  String? parkingYn; // 주자 여부
-  String? videoYn; // 영상촬영 여부
+
+  String? parkingYn = "가능"; // 주자 여부
+  String? videoYn = "가능"; // 영상촬영 여부
+  String? termsYn; // 약관동의
+  String _genre ="";
   List<String> genreList = []; // 공연 가능한 장르
+
 
   String? startTime; // 영업시간
   String? entTime; // 영업 시간
-  List<File> _commerImageList = []; // 상업공간 이미지
+  final List<File> _commerImageList = []; // 상업공간 이미지
 
   // 지도 API
   GoogleMapController? mapController;
@@ -66,6 +69,25 @@ class _ProprietorAddState extends State<ProprietorAdd> {
   int? _zip;
   String _addr= '';
   String _regions = '';
+
+
+
+
+
+  // 입력 쿼리문
+  void addGenresToFirestore(List<String> selectedGenres) {
+    // Firestore 데이터베이스 인스턴스를 가져옵니다.
+
+    // "genres" 컬렉션에 데이터 추가
+    fs.collection('genres').add({
+      'selectedGenres': selectedGenres,
+      // 다른 필드도 추가할 수 있습니다.
+    }).then((DocumentReference document) {
+      print('Document added with ID: ${document.id}');
+    }).catchError((error) {
+      print('Error adding document: $error');
+    });
+  }
 
   // 이미지
   Future<void> _pickImage() async {
@@ -113,14 +135,32 @@ class _ProprietorAddState extends State<ProprietorAdd> {
   Widget? _commerImageListWidget() {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    if (_commerImageList != null) {
+    if (_commerImageList.isEmpty) {
       // 이미지를 미리보기로 보여줄 수 있음
-      List<Widget> imageContainers = _commerImageList.map((image) {
-        return Container(
-          child: Image.file(image, height: screenHeight * 0.1, width: screenWidth * 0.2, fit: BoxFit.fill),
-        );
-      }).toList();
+      List<Widget> imageContainers = [];
 
+      for (int index = 0; index < _commerImageList.length; index++) {
+        final image = _commerImageList[index];
+        imageContainers.add(
+          Stack(
+            children: [
+              Image.file(image, height: screenHeight * 0.1, width: screenWidth * 0.2, fit: BoxFit.fill),
+              Positioned(
+                right: -13,
+                top: -13,
+                child: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _commerImageList.removeAt(index);
+                    });
+                  },
+                  icon: Icon(Icons.close),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
       return Wrap(
         spacing: 5.0,  // 이미지 사이의 가로 간격
         runSpacing: 0.1, // 이미지 사이의 세로 간격
@@ -132,6 +172,7 @@ class _ProprietorAddState extends State<ProprietorAdd> {
 
   @override
   void initState() {
+    super.initState();
     // TODO: implement initState
     _getCoordinatesFromAddress('서울 시청');
     
@@ -177,6 +218,190 @@ class _ProprietorAddState extends State<ProprietorAdd> {
     }
     return "주소를 찾을 수 없음";
   }
+
+  // 검색에 사용될 장르 라디오 버튼
+  Widget _customRadioBut() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        OutlinedButton(
+          onPressed: () {
+            setState(() {
+              _genre = '음악';
+            });
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+              if (_genre == '음악') {
+                return Color(0xFF392F31); // 선택된 경우의 색상
+              }
+              return Colors.white; // 선택되지 않은 경우의 색상
+            }),
+          ),
+          child: Text(
+            '음악',
+            style: TextStyle(
+              color: _genre == '음악' ? Colors.white : Color(0xFF392F31),
+            ),
+          ),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            setState(() {
+              _genre = '댄스';
+            });
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+              if (_genre == '댄스') {
+                return Color(0xFF392F31);
+              }
+              return Colors.white;
+            }),
+          ),
+          child: Text(
+            '댄스',
+            style: TextStyle(
+              color: _genre == '댄스' ? Colors.white : Color(0xFF392F31),
+            ),
+          ),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            setState(() {
+
+              _genre = '퍼포먼스';
+            });
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+              if (_genre == '퍼포먼스') {
+                return Color(0xFF392F31);
+              }
+              return Colors.white;
+            }),
+          ),
+          child: Text(
+            '퍼포먼스',
+            style: TextStyle(
+              color: _genre == '퍼포먼스' ? Colors.white : Color(0xFF392F31),
+            ),
+          ),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            setState(() {
+              _genre = '마술';
+            });
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+              if (_genre == '마술') {
+                return Color(0xFF392F31);
+              }
+              return Colors.white;
+            }),
+          ),
+          child: Text(
+            '마술',
+            style: TextStyle(
+              color: _genre == '마술' ? Colors.white : Color(0xFF392F31),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // 상세 장르 선택
+  Widget _wrapWidget(String genre) {
+    Map<String, List<String>> genreButtonMap = {
+      "음악": ["밴드", "발라드", "힙합", "클래식", "악기연주", "싱어송라이터"],
+      "댄스": ["팝핀", "비보잉", "힙합댄스", "하우스", "크럼프", "락킹", "왁킹"],
+      "퍼포먼스": ["행위예술", "현대미술"],
+    };
+
+    final buttonList = genreButtonMap[genre];
+    if (buttonList != null) {
+      return Wrap(
+        spacing: 5.0,
+        runSpacing: 0.1,
+        children: buttonList.map((label) {
+          bool isSelected = genreList.contains(label);
+          return OutlinedButton(
+            onPressed: () {
+              setState(() {
+                if (isSelected) {
+                  genreList.remove(label); // 이미 선택된 요소를 다시 클릭하면 리스트에서 제외
+                } else {
+                  genreList.add(label); // 새로운 요소를 선택
+                }
+              });
+            },
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25), // 둥근 모서리 반경 설정
+                ),
+              ),
+              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                EdgeInsets.all(8.0), // 버튼의 내부 여백 설정
+              ),
+              side: MaterialStateProperty.all<BorderSide>(
+                BorderSide(
+                  color: isSelected ? Color(0xFF392F31) : Colors.white, // 선택된 버튼인지 여부에 따라 테두리 색 변경
+                  width: 2.0, // 테두리 두께 설정
+                ),
+              ),
+            ),
+            child: Text(
+              label,
+              style: TextStyle(color: isSelected ? Color(0xFF392F31) : Colors.black), // 선택된 버튼일 때 텍스트 색상 변경
+            ),
+          );
+        }).toList(),
+
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  Widget genreListWidget() {
+    Map<String, List<String>> selectedGenres = {};
+    Map<String, List<String>> genreButtonMap = {
+      "음악": ["밴드", "발라드", "힙합", "클래식", "악기연주", "싱어송라이터"],
+      "댄스": ["팝핀", "비보잉", "힙합댄스", "하우스", "크럼프", "락킹", "왁킹"],
+      "퍼포먼스": ["행위예술", "현대미술"],
+    };
+
+    // 그룹화된 장르 목록 작성
+    for (String selectedGenre in genreList) {
+      for (String category in genreButtonMap.keys) {
+        if (genreButtonMap[category]!.contains(selectedGenre)) {
+          if (selectedGenres.containsKey(category)) {
+            selectedGenres[category]!.add(selectedGenre);
+          } else {
+            selectedGenres[category] = [selectedGenre];
+          }
+        }
+      }
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: selectedGenres.keys.map((category) {
+        return Padding(
+          padding: EdgeInsets.symmetric(vertical: 5.0),
+          child: Text(
+            '$category - ${selectedGenres[category]!.join("/")}',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        );
+      }).toList(),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -229,24 +454,22 @@ class _ProprietorAddState extends State<ProprietorAdd> {
                   Center(
                     child: _buildSelectedImage() ??
                           Container(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  border: Border.all(
-                                    color: Colors.black54,  // 테두리 색상 설정
-                                    width: 2.0,           // 테두리 두께 설정
-                                  ),
-                                ),
-                                height: screenHeight * 0.25, width: screenWidth * 0.5,
-                                child: OutlinedButton(
-                                  onPressed: _pickImage,
-                                  child: Image.asset(
-                                    'assets/fileAdd.png',
-                                    width: screenWidth * 0.2,
-                                    height: screenHeight * 0.1,
-                                  ),
-                                ),
-                              )
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              border: Border.all(
+                                color: Colors.black54,  // 테두리 색상 설정
+                                width: 2.0,           // 테두리 두께 설정
+                              ),
+                            ),
+                            height: screenHeight * 0.25, width: screenWidth * 0.5,
+                            child: OutlinedButton(
+                              onPressed: _pickImage,
+                              child: Image.asset(
+                                'assets/fileAdd.png',
+                                width: screenWidth * 0.2,
+                                height: screenHeight * 0.1,
+                              ),
+                            ),
                           ),
 
                   ),
@@ -341,6 +564,13 @@ class _ProprietorAddState extends State<ProprietorAdd> {
                         borderRadius: BorderRadius.circular(6))),
               ),
               Text("장르",style: subStyle,),
+              Column(
+                children: [
+                  _customRadioBut(),
+                  //_wrapWidget(_genre),
+                  genreListWidget()
+                ],
+              ),
               Text("주차",style: subStyle,),
               Row(
                 children: [
@@ -395,6 +625,21 @@ class _ProprietorAddState extends State<ProprietorAdd> {
                   Text("불가능"),
                 ],
               ),
+              Text("약관동의",style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),),
+              Row(
+                children: [
+                  Radio(
+                      value: "Y",
+                      groupValue: termsYn,
+                      onChanged: (value) {
+                        setState(() {
+                          termsYn = value;
+                        });
+                      }
+                  ),
+                  Text("사업주 이용약관(필수)")
+                ],
+              ),
             ],
           ),
         ),
@@ -410,7 +655,7 @@ class _ProprietorAddState extends State<ProprietorAdd> {
         children: [
           Text('주소'),
           SizedBox(height: 20),
-          Container(
+          SizedBox(
             height: 200,
             child: coordinates == null
                 ? Container()
@@ -436,7 +681,7 @@ class _ProprietorAddState extends State<ProprietorAdd> {
             ),
           ),
           SizedBox(height: 20),
-          Container(
+          SizedBox(
             height: 35,
             child: TextField(
               style: TextStyle(
@@ -454,7 +699,7 @@ class _ProprietorAddState extends State<ProprietorAdd> {
                 _getCoordinatesFromAddress(value);
               },
             ),
-          )
+          ),
         ],
       ),
     );
@@ -467,9 +712,9 @@ class _ProprietorAddState extends State<ProprietorAdd> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start, // 수직 가운데 정렬 설정
         children: [
-          Text('$title'),
+          Text(title),
           SizedBox(height: 10),
-          Container(
+          SizedBox(
             height: 35,
             child: TextField(
               style: TextStyle(
@@ -478,7 +723,7 @@ class _ProprietorAddState extends State<ProprietorAdd> {
               controller: control,
               decoration: InputDecoration(
                 contentPadding: EdgeInsets.only(left: 10),
-                hintText: '$hint',
+                hintText: hint,
                 hintStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
                 border: OutlineInputBorder(),
               ),
